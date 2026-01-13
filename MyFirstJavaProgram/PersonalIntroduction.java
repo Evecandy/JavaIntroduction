b9b384b6ae726efba9a7119260f26c47355fc024
candy <evecandymutunga@gmail.com>
2026-01-13 04:25:32 +0300

    update goal message in PersonalIntroduction.java to include emojis

--- MyFirstJavaProgram/PersonalIntroduction.java
+++ MyFirstJavaProgram/PersonalIntroduction.java
@@ -3,7 +3,7 @@ package MyFirstJavaProgram;
 public class PersonalIntroduction {
     public static void main(String[] args) {
         System.out.println("My Name: Eve Candy");
-        System.out.println("My Goals: Become a certified Java developer in 3 months");
+        System.out.println("My Goals: Become a certified Java developer in 3 months✨✨✨");
         System.out.println("Excited to learn: Algorithms and Data Structures in Java");
     }
 }
